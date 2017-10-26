59dd8fd24fa33c10fa13970a21ac1a85206c8268
Luiz Otávio Dorigon <luiz.otavio.dorigon@gmail.com>
2017-10-26 10:24:16 -0200

    Fixed bug scroll first time.

--- app/src/main/java/br/com/dorigon/testecollapseexpand/MainActivity.java
+++ app/src/main/java/br/com/dorigon/testecollapseexpand/MainActivity.java
@@ -1,16 +1,13 @@
 package br.com.dorigon.testecollapseexpand;
 
 import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.Transformation;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
@@ -19,27 +16,25 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private LinearLayout mLayoutTop, mLayoutBottom;
-    private RecyclerView mRecyclerView;
 
-    private int offset = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = findViewById(R.id.recyclerView_names);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView_names);
         mLayoutTop = findViewById(R.id.linearLayout_top);
         mLayoutBottom = findViewById(R.id.linearLayout_bottom);
 
-        mRecyclerView.setHasFixedSize(Boolean.TRUE);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-        mRecyclerView.setAdapter(new RecyclerViewAdapter(this, getNames()));
-        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+        recyclerView.setHasFixedSize(Boolean.TRUE);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(new RecyclerViewAdapter(this, getNames()));
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
-                Log.i("dy",dy+"");
+                Log.i("dy", dy + "");
                 collapse(dy);
 
             }
@@ -48,11 +43,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void collapse(int y) {
         if (y > 0) {
-            expand(mLayoutBottom,y);
-            collapse(mLayoutTop,y);
-        } else if (y < 0){
-            collapse(mLayoutBottom,y*-1);
-            expand(mLayoutTop,y*-1);
+            expand(mLayoutBottom, y);
+            collapse(mLayoutTop, y);
+        } else if (y < 0) {
+            collapse(mLayoutBottom, y * -1);
+            expand(mLayoutTop, y * -1);
         }
     }
 
@@ -64,27 +59,27 @@ public class MainActivity extends AppCompatActivity {
         return names;
     }
 
-    public static  int dpToPx(int px) {
+    public static int dpToPx(int px) {
         return (int) (px * Resources.getSystem().getDisplayMetrics().density);
     }
 
-    public static void expand(final View v,int y) {
+    public static void expand(final View v, int y) {
         final int targetHeight = dpToPx(80);
-        if (v.getLayoutParams().height < targetHeight){
-            if (v.getLayoutParams().height + y > targetHeight)
+        if (v.getLayoutParams().height < targetHeight) {
+            if (v.getLayoutParams().height + y > targetHeight) {
                 v.getLayoutParams().height = targetHeight;
-            else{
+            } else {
                 v.getLayoutParams().height += y;
             }
             v.requestLayout();
         }
     }
 
-    public static void collapse(final View v,int y) {
-        if (v.getLayoutParams().height > 0){
-            if (v.getLayoutParams().height - y < 0)
+    public static void collapse(final View v, int y) {
+        if (v.getLayoutParams().height > 0) {
+            if (v.getLayoutParams().height - y < 0) {
                 v.getLayoutParams().height = 0;
-            else{
+            } else {
                 v.getLayoutParams().height -= y;
             }
             v.requestLayout();
